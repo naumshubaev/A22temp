b4f3353298357a3cccce235a3b852d6b215f3264
naumshubaev <naum.shubaev@gmail.com>
2022-04-10 23:27:44 +0300

    fully funtional test

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -2,24 +2,45 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.withText;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDelivery {
+
+    String deliveryDate() {
+        // deliveryDate == today+daysAdded
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        int daysAdded = 3;
+        // daysAdded выглядит, наверное, как лишняя переменная (если уж переменная - то лучше в самом тесте, а тут можно было просто значение подставить)
+        // но я реализовал так, чтобы потом сделать рандомайзер, т.е. чтобы deliveryDate возвращал
+        // случайную дату, подходящую под ТЗ и в нужном формате
+        // считаю, что ТЗ в ДЗ соответствует и в таком виде - "не ранее трёх дней с текущей даты"
+
+        return today.plusDays(daysAdded).format(dtf);
+    }
+
     @Test
     public void shouldOrderCardDelivery() {
         // do we need to hold it open?
- //       Configuration.headless = true;
- //       Configuration.holdBrowserOpen = true;
-        open("http://0.0.0.0:9999");
-        $("[data-test-id=\"city\"]").setValue("Москва");
-// ??        $(withText("Москва")).shouldBe(Condition.visible);
-
-//        $("[data-test-id=\"city\"]");
-        //autoconverted from $('[data-test-id="city"]')
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate());
+        $("[data-test-id='name'] input").setValue("Василий Пупкин");
+        $("[data-test-id='phone'] input").val("+78121234567");
+        $("[data-test-id='agreement']>span").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[class='notification__content']")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + "13.04.2022")
+                        , Duration.ofSeconds(15));
     }
 }
